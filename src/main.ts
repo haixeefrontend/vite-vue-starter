8e1c366e67700fba073a97b3f9b6131c7c918503
Maiko Tan <maiko.tan.coding@gmail.com>
2024-06-21 11:53:37 +0800

    feat: add pinia store

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
+
 import { router } from './routes'
+import { pinia } from './store'
 import './style.scss'
 import App from './App.vue'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(pinia).mount('#app')
