dc79e269c1801d84a1cba4d83bd5090abfaa7b45
Maiko Tan <maiko.tan.coding@gmail.com>
2024-06-21 13:37:00 +0800

    feat: add unocss

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { createApp } from 'vue'
 
+import App from './App.vue'
 import { router } from './routes'
 import { pinia } from './store'
+
 import './style.scss'
-import App from './App.vue'
+import '@unocss/reset/tailwind.css'
+import 'virtual:uno.css'
 
 createApp(App).use(router).use(pinia).mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
+
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vue from '@vitejs/plugin-vue'
+import { presetUno } from 'unocss'
+import uno from 'unocss/vite'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [vue(), vueJsx(), uno({ presets: [presetUno()] })],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
