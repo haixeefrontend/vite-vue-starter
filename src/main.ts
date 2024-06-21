8cb55b1b6cc1e1938511689f3de09a5ffa3fd749
Maiko Tan <maiko.tan.coding@gmail.com>
2024-06-21 13:56:04 +0800

    refa: migrate style to scss

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import { router } from './routes'
-import './style.css'
+import './style.scss'
 import App from './App.vue'
 
 createApp(App).use(router).mount('#app')
