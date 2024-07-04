df4efdd7e7461c9ef2042ccff68fbbc177932674
Maiko Tan <maiko.tan.coding@gmail.com>
2024-07-04 10:36:58 +0800

    fix: separate unocss config out of vite's

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,12 @@ import { fileURLToPath, URL } from 'node:url'
 
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vue from '@vitejs/plugin-vue'
-import { presetUno } from 'unocss'
 import uno from 'unocss/vite'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), uno({ presets: [presetUno()] })],
+  plugins: [vue(), vueJsx(), uno()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
