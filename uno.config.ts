780c1f2a952fb258570973a428149f3759680ed6
Maiko Tan <maiko.tan.coding@gmail.com>
2024-07-05 17:12:15 +0800

    fix: revert unocss to previous config file
    Need to upgrade unocss VSCode extension to 0.61.2 version

--- uno.config.ts
+++ uno.config.ts
@@ -1,9 +1,9 @@
 import { presetHaixee } from '@haixee/unocss-preset'
-import { defineConfig, presetUno, transformerVariantGroup } from 'unocss'
+import { defineConfig, presetUno, transformerDirectives, transformerVariantGroup } from 'unocss'
 
 export default defineConfig({
   presets: [presetUno(), presetHaixee()],
-  transformers: [transformerVariantGroup()],
+  transformers: [transformerVariantGroup(), transformerDirectives()],
   shortcuts: {
     responsive: 'w-full h-full px-4 lg:(mx-auto max-w-1200px)'
   }
