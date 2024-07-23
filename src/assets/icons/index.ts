65b2e8cda32d51e89e4abef782a965625dabd959
Maiko Tan <maiko.tan.coding@gmail.com>
2024-07-23 10:06:10 +0800

    feat: use element-plus for better header style

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,7 +1,5 @@
-import Close from './close.vue'
 import Menu from './menu.vue'
 
 export {
-  Close,
   Menu,
 }
--- src/components/layout/header.vue
+++ src/components/layout/header.vue
@@ -3,8 +3,7 @@
     <div class="responsive flex items-center">
       <img src="#" class="transition-(! max-width) <md:hidden md:max-w-45 lg:max-w-60" />
       <icon-menu :class="['md:hidden size-8 transition', { 'opacity-0': showMenu }]" @click="showMenu = !showMenu" />
-      <icon-close :class="['md:hidden size-8 transition', { 'opacity-0': !showMenu }]" @click="showMenu = !showMenu" />
-      <nav class="<md:relative flex-1 nav" :data-active="showMenu">
+      <nav class="<md:hidden flex-1 nav" :data-active="showMenu">
         <ul>
           <template v-for="link in headers" :key="link.name">
             <li class="w-20 py-4 text-center">
@@ -13,15 +12,27 @@
           </template>
         </ul>
       </nav>
+
+      <el-drawer v-model="showMenu" direction="ltr" size="70%">
+        <ul class="w-full flex-(~ col center)">
+          <template v-for="link in headers" :key="link.name">
+            <li class="w-20 py-4 text-center" @click="showMenu = !showMenu">
+              <router-link :to="{ name: link.name }">{{ link.meta.title }}</router-link>
+            </li>
+          </template>
+        </ul>
+      </el-drawer>
     </div>
   </header>
 </template>
 
 <script setup lang="ts">
+import { ElDrawer } from 'element-plus'
 import { ref } from 'vue'
 import { RouterLink } from 'vue-router'
+
+import { Menu as IconMenu } from '@/assets/icons'
 import { headers } from '@/routes'
-import { Close as IconClose, Menu as IconMenu } from '@/assets/icons'
 
 defineOptions({
   name: 'ComponentLayoutHeader',
