75d6f6a8956d1561f031f5032eb88cb5840fbc65
Maiko Tan <maiko.tan.coding@gmail.com>
2024-07-23 09:56:03 +0800

    feat: add element-plus and override feature

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,23 @@ import { fileURLToPath, URL } from 'node:url'
 
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vue from '@vitejs/plugin-vue'
+import ElementPlus from 'unplugin-element-plus/vite'
 import uno from 'unocss/vite'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), uno()],
+  plugins: [vue(), vueJsx(), uno(), ElementPlus({ defaultLocale: 'zh-cn', useSource: true })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/styles/element/index.scss" as *;`,
+      },
     },
   },
 })
