573fe25bbb66ba7d9be20d171af833b0c6c3fa12
Maiko Tan <maiko.tan.coding@gmail.com>
2024-07-31 16:01:52 +0800

    feat: optimise footer

--- src/components/layout/footer.vue
+++ src/components/layout/footer.vue
@@ -1,6 +1,13 @@
 <template>
   <footer class="sticky top-100vh py-8">
-    <p>&copy; 2021</p>
+    <div class="text-center">
+      <a class="text-sm" href="http://beian.miit.gov.cn" target="_blank">备案号</a>
+      <span> | </span>
+      <!-- TODO: replace the beian number and link -->
+      <a class="text-sm" href="#" target="_blank">
+        公安备案
+      </a>
+    </div>
   </footer>
 </template>
 
