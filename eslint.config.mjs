a1d422e942b20302808ba9ea67a77d96f83c0826
Maiko Tan <maiko.tan.coding@gmail.com>
2025-12-31 11:39:35 +0800

    feat: add auto import and components plugin supports
    Signed-off-by: Maiko Tan <maiko.tan.coding@gmail.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,14 @@
 import haixee from '@haixee/eslint-config'
 
+import autoImportIgnores from './.eslintrc-auto-import.json' with { type: 'json' }
+
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   ...haixee.configs.typescript,
   ...haixee.configs.vue,
   {
     languageOptions: {
+      ...autoImportIgnores,
       parserOptions: {
         projectService: {
           allowDefaultProject: ['eslint.config.mjs'],
--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,45 @@ import { fileURLToPath, URL } from 'node:url'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import uno from 'unocss/vite'
+import AutoImport from 'unplugin-auto-import/vite'
 import ElementPlus from 'unplugin-element-plus/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import Components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx(), uno(), ElementPlus({ defaultLocale: 'zh-cn', useSource: true })],
+  plugins: [
+    vue(),
+    vueJsx(),
+    uno(),
+    // https://unplugin.unjs.io/showcase/unplugin-auto-import.html#configuration
+    AutoImport({
+      imports: [
+        'vue',
+
+        // Auto-Import Custom Packages
+        {
+          // Project Custom APIs
+          '@/api': [
+            'useApi',
+          ],
+        },
+      ],
+      dts: true,
+      eslintrc: {
+        enabled: true,
+      },
+    }),
+    Components({
+      dirs: ['src/components'],
+      dts: true,
+      resolvers: [
+        ElementPlusResolver(),
+      ],
+    }),
+    ElementPlus({ defaultLocale: 'zh-cn', useSource: true }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
@@ -21,7 +54,7 @@ export default defineConfig({
          * For use `sass-embedded` instead of `sass`
          * `sass-embedded` is binary version of `sass` which is faster
          */
-        api: 'modern-compiler',
+        api: 'modern',
         additionalData: `@use "@/styles/element/index.scss" as *;`,
       },
     },
